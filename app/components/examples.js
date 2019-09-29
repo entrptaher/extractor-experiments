d898e84d8c792b83ec0dd393eda5bc45e6dca72e
entrptaher@gmail.com <entrptaher@gmail.com>
2019-09-29 21:59:29 +0600

    init

--- app/components/examples.js
+++ app/components/examples.js
@@ -4,17 +4,6 @@ import Tabs from './tabs'
 
 const Examples = () => (
   <div>
-    <h1>Toggle Example</h1>
-    <div id="toggle">
-      <div className="firstTwo">
-        <Toggle name="One" apple/>
-        <Toggle name="Two" mango/>
-      </div>
-      <div className="lastTwo">
-        <Toggle name="Three" mango apple/>
-        <Toggle name="Four" apple/>
-      </div>
-    </div>
     <h1>Non-Toggle Example</h1>
     <div id="toggle">
       <div className="firstTwo">
@@ -24,6 +13,17 @@ const Examples = () => (
       <div className="lastTwo">
         <Toggle name="Three" mango apple alwaysShow/>
         <Toggle name="Four" apple alwaysShow/>
+      </div>
+    </div>
+    <h1>Toggle Example</h1>
+    <div id="toggle">
+      <div className="firstTwo">
+        <Toggle name="One" apple/>
+        <Toggle name="Two" mango/>
+      </div>
+      <div className="lastTwo">
+        <Toggle name="Three" mango apple/>
+        <Toggle name="Four" apple/>
       </div>
     </div>
     <h1>Tab Example</h1>
